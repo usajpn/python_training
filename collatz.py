a7210010e3c9bef794402ae5ee698b2398b73b72
Shin Usami <usa@ht.sfc.keio.ac.jp>
2013-04-16 20:34:00 +0900

    fix bug and unpythontic parts in collatz.py

--- collatz.py
+++ collatz.py
@@ -1,16 +1,22 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
+
 def collatz(num):
     counter = 0
 
-    while num != 1:
+    while True:
         counter += 1
-        if num % 2 == 0:
+
+        if num == 1:
+            break
+        elif num % 2 == 0:
             num = num / 2
         else:
             num = 3 * num + 1
+
     return counter
+
 
 def findMax(minScope, maxScope):
     maxSeqNum = 0
@@ -19,19 +25,21 @@ def findMax(minScope, maxScope):
 
     for num in range(minScope, maxScope):
         tmpLen = collatz(num)
-        if (tmpLen > maxSeqLen):
+        if tmpLen > maxSeqLen:
             maxSeqLen = tmpLen
             maxSeqNum = num
 
     return maxSeqNum, maxSeqLen
 
+
 def printNumbers(num, len):
     print "Maximum Sequence Number: " + str(num)
     print "Maximum Sequence Length: " + str(len)
 
+
 if __name__ == "__main__":
     minScope = 2
     maxScope = 1000000
+
     maxSeqNum, maxSeqLen = findMax(minScope, maxScope)
     printNumbers(maxSeqNum, maxSeqLen)
-
